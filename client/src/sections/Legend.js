d505cb550675d2314f975e0d2c23c292b154ff8b
15matias15 <matpereirag@gmail.com>
2018-12-24 19:12:56 +0100

    functions adjustment (ES6)

--- client/src/sections/Legend.js
+++ client/src/sections/Legend.js
@@ -5,33 +5,31 @@ import { faPlus, faSquare, } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faPlus, faSquare);
 
-const legend = () => {
-    return (
-        <div className="col-sm-6" >
-            <h5><strong>Legend</strong></h5>
-            <label className="legend-label">
-                <FontAwesomeIcon
-                    icon={faSquare}
-                    className="fa-xs "
-                    style={{ color: "grey", width: 15, height: 15 }} />
-                <p>&emsp;Images or add image button</p>
-            </label>
-            <label className="legend-label">
-                <FontAwesomeIcon
-                    icon={faPlus}
-                    className="fa-xs"
-                    style={{ color: "green", width: 15, height: 15 }} />
-                <p> &emsp;Add row/column buttons</p>
-            </label>
-            <label className="legend-label">
-                <FontAwesomeIcon
-                    icon={faPlus}
-                    className="fa-xs"
-                    style={{ color: "grey", width: 15, height: 15 }} />
-                <p>&emsp;Add image</p>
-            </label>
-        </div>
-    )
-}
+const legend = () => (
+    <div className="col-sm-6" >
+        <h5><strong>Legend</strong></h5>
+        <label className="legend-label">
+            <FontAwesomeIcon
+                icon={faSquare}
+                className="fa-xs "
+                style={{ color: "grey", width: 15, height: 15 }} />
+            <p>&emsp;Images or add image button</p>
+        </label>
+        <label className="legend-label">
+            <FontAwesomeIcon
+                icon={faPlus}
+                className="fa-xs"
+                style={{ color: "green", width: 15, height: 15 }} />
+            <p> &emsp;Add row/column buttons</p>
+        </label>
+        <label className="legend-label">
+            <FontAwesomeIcon
+                icon={faPlus}
+                className="fa-xs"
+                style={{ color: "grey", width: 15, height: 15 }} />
+            <p>&emsp;Add image</p>
+        </label>
+    </div>
+)
 
 export default legend;
--- client/src/sections/Notes.js
+++ client/src/sections/Notes.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-const notes = () => {
-    return (
-        <div className="col-sm-6">
-            <h5><strong>Notes</strong></h5>
-            <p><span style={{ fontStyle: 'italic' }}>Italic </span>text is editable</p>
-            <p>You can only upload file in format JPG,JPEG,PNG,PDF,DOC & DOCX</p>
-        </div>
-    )
-}
+const notes = () => (
+    <div className="col-sm-6">
+        <h5><strong>Notes</strong></h5>
+        <p><span style={{ fontStyle: 'italic' }}>Italic </span>text is editable</p>
+        <p>You can only upload file in format JPG,JPEG,PNG,PDF,DOC & DOCX</p>
+    </div>
+)
 
 export default notes;
